bf1f2daade9b74a321a742ddd69c833c5286e5bc
Mario Dujić <liberasoftware@gmail.com>
2020-04-18 22:17:53 +0200

    added navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,9 +2,11 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from '@material-ui/icons/Menu';
+import {AccountCircle} from "@material-ui/icons";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = {
   root: {
@@ -19,17 +21,63 @@ const useStyles = {
 }
 
 class Navigation extends React.Component {
+
+  state = {
+    anchorEl: null
+  }
+
+  handleMenu = (event) => {
+    this.setState({
+      anchorEl: event.currentTarget
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      anchorEl: null
+    })
+  };
+
   render() {
+    const open = Boolean(this.state.anchorEl)
+
     return <div style={useStyles.root}>
-      <AppBar position="static" color="primary">
+      <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" style={useStyles.menuButton} color="inherit" arsia-label="menu">
+          <IconButton edge="start" style={useStyles.menuButton} color="inherit" aria-label="menu">
             <MenuIcon/>
           </IconButton>
           <Typography variant="h5" style={useStyles.title}>
             {this.props.title}
           </Typography>
-          <Button color="inherit">Login</Button>
+          <div>
+            <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit">
+              <AccountCircle/>
+            </IconButton>
+            <Menu
+                id="menu-appbar"
+                ref={this.wrapper}
+                anchorEl={this.state.anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={this.handleClose}>
+              <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+              <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
